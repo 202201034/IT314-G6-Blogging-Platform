45b880a8c2f876bef471b54cbd1db76fa10a9b86
Denil <denilantala21@gmail.com>
2024-09-28 18:10:15 +0530

    "Added logout functionality and redirect to home page after login."

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,4 +1,9 @@
 import localFont from "next/font/local";
+import { GoSignOut } from "react-icons/go";
+import { useAuth } from "@/firebase/auth";
+import { useRouter } from "next/router";
+import Loader from "./components/Loader";
+import { useEffect } from "react";
 require('dotenv').config();
 
 const geistSans = localFont({
@@ -13,9 +18,27 @@ const geistMono = localFont({
 });
 
 export default function Index() {
+  const {authUser, isLoading, signOut} = useAuth();
+
+    const router = useRouter();
+
   return (
     <main>
       <h1 className="text-center py-4 text-xl">Welcome to Our Blogging Website!</h1>
+
+      {!authUser && (
+        <div className="bg-black text-white w-44 py-4 mt-10 rounded-lg transition-transform hover:bg-black/[0.8] active:scale-90 flex items-center justify-center gap-2 font-medium shadow-md fixed bottom-5 right-5 cursor-pointer" onClick={()=>router.push('../login')}>
+          <GoSignOut size={18} />
+          <span>Login</span>
+        </div>
+      )}
+
+      {authUser && (
+        <div className="bg-black text-white w-44 py-4 mt-10 rounded-lg transition-transform hover:bg-black/[0.8] active:scale-90 flex items-center justify-center gap-2 font-medium shadow-md fixed bottom-5 right-5 cursor-pointer" onClick={signOut}>
+          <GoSignOut size={18} />
+          <span>Logout</span>
+        </div>
+      )}
     </main>
   )
 }
