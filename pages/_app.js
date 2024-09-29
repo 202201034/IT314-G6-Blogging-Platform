d0d384a176c78a257eab9634d524602dc2ae6c69
202201037 <202201037@daiict.ac.in>
2024-09-29 13:16:22 +0530

    Added layout to _app.js and updated index.jsx

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,16 @@
-import '../styles/globals.css';
-import { AuthUserProvider } from "@/firebase/auth"
-
-export default function App({ Component, pageProps }) {
+import "@/styles/globals.css";
+import Layout from './components/layout'
+import { config } from '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/fontawesome-svg-core/styles.css'; // Import the CSS
+import { AuthUserProvider } from "@/firebase/auth";
+config.autoAddCss = false; 
+ 
+export default function MyApp({ Component, pageProps }) {
   return (
-    <>
     <AuthUserProvider>
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </AuthUserProvider>
-    </>
-  );
-  }
-  
+  )
+}
